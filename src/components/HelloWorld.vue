7d13a924ba074d087a143234bf8dac0bef05f4a5
喻希里 <293730698@qq.com>
2020-11-20 01:24:00 +0800

    feature: a home page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,52 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <div class="content">
+      <div class="header">
+        <div class="logo">
+          <img src="../assets/logo-icon.png" />
+        </div>
+        <div class="call-me">
+          <img src="../assets/home.gif" />
+          <img src="../assets/email.gif" />
+        </div>
+      </div>
+      <div class="content-title">
+        请选择对应项目？
+      </div>
+      <div @click="onCardClick($event)" class="content-card">
+        <div class="card">
+          <img data-url="http://www.baidu.com" src="../assets/logo-icon.png">
+        </div>
+        <div class="card">
+          <img data-url="data-url-yy" src="../assets/logo-icon.png">
+        </div>
+        <div class="card">
+          <img data-url="data-url-yy" src="../assets/logo-icon.png">
+        </div>
+        <div class="card">
+          <img src="../assets/logo-icon.png">
+        </div>
+        <div class="card">
+          <img src="../assets/logo-icon.png">
+        </div>
+        <div class="card">
+          <img src="../assets/logo-icon.png">
+        </div>
+        <div class="card">
+          <img src="../assets/logo-icon.png">
+        </div>
+        <div class="card">
+          <img src="../assets/logo-icon.png">
+        </div>
+      </div>
+      <div class="footer">
+        <div class="text fts20">第一行中文第一行中文第一行中文第一行中文第一行中文第一行中文</div>
+        <div class="text fts15">second row text second row text second row text second row text </div>
+        <div class="text mgt fts20">第三行中文第三行中文第三行中文第三行中文第三行中文</div>
+        <div class="text fts15">fourth row text fourth row text fourth row text</div>
+        <div class="text mgt fts20">备案号备案号备案号</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -35,24 +55,114 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  methods: {
+    onCardClick: function (e) {
+      const {
+        toElement: {
+          dataset: {
+            url
+          }
+        }
+      } = e;
+      window.location.href = url;
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .mgt {
+    margin-top: 8px;
+  }
+  .fts20 {
+    font-size: 16px;
+  }
+  .fts15 {
+    font-size: 10px;
+  }
+  .container {
+    background-color: #101010;
+    display: flex;
+    justify-content: center;
+    font-family:  -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";;
+  }
+  .content {
+    width: 766px;
+    height: 100%;
+    background-image: url('../assets/content-bg.jpg');
+  }
+  .header {
+    width: 100%;
+    height: 111px;
+    /* background-color: aliceblue; */
+    display: flex;
+    justify-content: space-between;
+  }
+  .content-title {
+    width: 100%;
+    height: 70px;
+    /* background-color: antiquewhite; */
+    font-size: 25px;
+    color: #ffffff;
+    text-align: left;
+    line-height: 70px;
+  }
+  .content-card {
+    width: 100%;
+    height: 265px;
+    /* background-color: cornflowerblue; */
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-content: space-between;
+  }
+  .content-card .card {
+    width: 185px;
+    height: 128px;
+    background-color: red;
+  }
+  .content-card .card img {
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+  .footer {
+    width: 100%;
+    height: 171px;
+    /* background-color: fuchsia; */
+    display: flex;
+    align-content: center;
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+  .footer .text {
+    display: block;
+    width: 100%;
+    text-align: center;
+    font-weight: bolder;
+    color: #e0dede;
+  }
+  .call-me {
+    display: flex;
+    width: 60px;
+    align-items: center;
+    justify-content: space-between;
+    margin-right: 50px;
+  }
+  .logo {
+    width: 184px;
+  }
+  .logo img {
+    /* width: 200px; */
+    width: 100%;
+    display: block;
+  }
+  .call-me img {
+    display: block;
+    /* width: 200px; */
+    width: 15px;
+    height: 15px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vueRouter from 'vue-router'
 
 Vue.config.productionTip = false
-
+Vue.use(vueRouter);
+const router = new vueRouter({
+  routes: [
+    {
+      name: 'home', path: '/home', component: null
+    }
+  ]
+})
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
